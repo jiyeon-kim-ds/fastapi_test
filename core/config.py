a22b81b1753b1192c6457396549123bc795977c9
Jiyeon Kim <jiyeon.kim.ds@gmail.com>
2023-01-13 11:25:10 +0900

    Add MySQL settings

---
+++ core/config.py
@@ -0,0 +1,14 @@
+import os
+
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    mysql_host: str = os.environ.get("MYSQL_HOST")
+    mysql_username: str = os.environ.get("MYSQL_USERNAME")
+    mysql_password: str = os.environ.get("MYSQL_PASSWORD")
+    mysql_database: str = os.environ.get("MYSQL_DATABASE")
+    mysql_port: int = os.environ.get("MYSQL_PORT")
+
+
+settings = Settings()
---
+++ database/session.py
@@ -0,0 +1,11 @@
+from sqlalchemy     import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from core.config import settings
+
+
+SQLALCHEMY_DATABASE_URL = f'mysql+mysqlconnector://{settings.mysql_username}:{settings.mysql_password}@{settings.mysql_server_name}:{settings.mysql_port}/{settings.mysql_database}'
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
