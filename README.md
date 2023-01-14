f5137cc2c08ef6f6ea19dda587f562b028e4312d
Jiyeon Kim <jiyeon.kim.ds@gmail.com>
2023-01-14 14:13:58 +0900

    Add Ledger table

--- README.md
+++ README.md
@@ -8,10 +8,9 @@ This FastAPI project is for test
 1. 수입/지출 (amount)
     > 수입과 지출을 같은 컬럼에 두고 +,-로 표기할 지 각자의 컬럼을 갖게 할 지 고민
    - 다른 컬럼: null 값이 많이 생김, 디스크를 많이 차지함
-   - 같은 컬럼: sum을 구하거나 할 때 용이 
-    
+   - 같은 컬럼: sum을 구하거나 할 때 용이
    -> 둘을 같은 컬럼에 두는게 좋을 듯
 2. 이름 (item)
 3. 메모 (note)
 4. event_date (거래 발생 날짜)
-5. created_at (transaction 생성 날짜)
+5. created_at (ledger 생성 날짜)
--- database/models.py
+++ database/models.py
@@ -1,17 +1,30 @@
-from sqlalchemy     import Column, DateTime, String, Integer, DateTime
-from sqlalchemy.orm import declarative_base
+from sqlalchemy     import Column, String, Integer, DateTime, ForeignKey, Text
+from sqlalchemy.orm import declarative_base, relationship
 from sqlalchemy.sql import func
 
 Base = declarative_base()
 
 
-class PrimaryKey:
-    id = Column(Integer, primary_key=True, index=True, unique=True)
+class PrimaryKey(Base):
+    id = Column(Integer, primary_key=True, unique=True)
 
 
-class User(Base, PrimaryKey):
+class User(PrimaryKey):
     __tablename__ = 'users'
 
     username    = Column(String(255), nullable=False, unique=True)
     password    = Column(String(255), nullable=False)
     joined_date = Column(DateTime(), server_default=func.now())
+    ledgers     = relationship('Ledger', back_populates='author')
+
+
+class Ledger(PrimaryKey):
+    __tablename__ = 'ledgers'
+
+    author_id  = Column(Integer, ForeignKey('user.id', ondelete='CASCADE'))
+    author     = relationship('User', back_populates='ledgers')
+    item       = Column(String(255))
+    note       = Column(Text())
+    amount     = Column(Integer(), nullable=False)
+    event_date = Column(DateTime())  # 거래 발생 날짜
+    created_at = Column(DateTime(), server_default=func.now())  # ledger 생성 날짜
