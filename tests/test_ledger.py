a800208bb54f88a3dbc1228fa10258b0079fc105
Jiyeon Kim <jiyeon.kim.ds@gmail.com>
2023-01-14 20:40:46 +0900

    Add create transaction feature

--- tests/test_ledger.py
+++ tests/test_ledger.py
@@ -1,18 +1,17 @@
 from datetime import datetime
 
-from tests.conftest  import client, TestingSessionLocal, get_user_token
+from tests.conftest  import client, get_user_token
 
 
-db = TestingSessionLocal()
 token_header = get_user_token()
 
 
-def test_create_ledger():
+def test_create_ledger(capsys):
     ledger_data = {
         "item": "꽤나 큰 지출",
         "note": "너무 큰 지출을 해버렸다!",
         "amount": -1000000,
-        "event_date": str(datetime.now())
+        "event_date": str(datetime.now().date())
     }
 
     response = client.post(
@@ -20,5 +19,8 @@ def test_create_ledger():
         json=ledger_data,
         headers=token_header
     )
+
+    with capsys.disabled():
+        print(response.json())
 
     assert response.status_code == 201
