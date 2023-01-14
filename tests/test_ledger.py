523b56cb5c54840e58c06a55f5edc9f5b2f142ab
Jiyeon Kim <jiyeon.kim.ds@gmail.com>
2023-01-14 20:44:22 +0900

    Delete unncessary lines

--- tests/test_ledger.py
+++ tests/test_ledger.py
@@ -6,7 +6,7 @@ from tests.conftest  import client, get_user_token
 token_header = get_user_token()
 
 
-def test_create_ledger(capsys):
+def test_create_ledger():
     ledger_data = {
         "item": "꽤나 큰 지출",
         "note": "너무 큰 지출을 해버렸다!",
@@ -19,8 +19,5 @@ def test_create_ledger(capsys):
         json=ledger_data,
         headers=token_header
     )
-
-    with capsys.disabled():
-        print(response.json())
 
     assert response.status_code == 201
