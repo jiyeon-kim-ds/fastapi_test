3c2d504539ad3bc7c18576a0c597501f55c5a7ab
Jiyeon Kim <jiyeon.kim.ds@gmail.com>
2023-01-15 13:25:13 +0900

    Add Redis settings

--- core/config.py
+++ core/config.py
@@ -1,5 +1,7 @@
 import os
 
+import redis
+
 from pydantic import BaseSettings
 
 
@@ -7,9 +9,22 @@ class Settings(BaseSettings):
     mysql_username: str = os.environ.get("MYSQL_USERNAME")
     mysql_password: str = os.environ.get("MYSQL_PASSWORD")
     mysql_database: str = os.environ.get("MYSQL_DATABASE")
-    mysql_host:     str = os.environ.get("MYSQL_HOST")
-    mysql_port:     int = os.environ.get("MYSQL_PORT")
-    auth_secret:    str = os.environ.get("AUTH_SECRET")
+    mysql_host    : str = os.environ.get("MYSQL_HOST")
+    mysql_port    : int = os.environ.get("MYSQL_PORT")
+    auth_secret   : str = os.environ.get("AUTH_SECRET")
+    redis_database: int = os.environ.get("REDIS_DATABASE")
+    redis_host    : str = os.environ.get("REDIS_HOST")
+    redis_port    : int = os.environ.get("MYSQL_PORT")
 
 
 settings = Settings()
+
+
+def load_redis():
+    return redis.StrictRedis(
+                            host=settings.redis_host,
+                            port=settings.redis_port,
+                            db=settings.redis_database,
+                            charset="utf-8",
+                            decode_responses=True
+                         )
